db5dbd95f0d5826218b96b526afe9e8dc9e10745
junwon <esanzy87@gmail.com>
2014-07-12 15:15:17 +0900

    Iteration 1 Complete

--- app/src/main/java/com/msk/adopt4k/LoginActivity.java
+++ app/src/main/java/com/msk/adopt4k/LoginActivity.java
@@ -4,6 +4,9 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
+import android.view.animation.LinearInterpolator;
 import android.widget.TextView;
 
 public class LoginActivity extends Activity {
@@ -13,10 +16,22 @@ public class LoginActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        final Animation animation = new AlphaAnimation(1, 0.3f); // Change alpha from fully visible to invisible
+        animation.setDuration(1500); // duration - half a second
+        animation.setInterpolator(new LinearInterpolator()); // do not alter animation rate
+        animation.setRepeatCount(Animation.INFINITE); // Repeat animation infinitely
+        animation.setRepeatMode(Animation.REVERSE); // Reverse animation at the end so the button will fade back in
+
         TextView signIn = (TextView)findViewById(R.id.signin);
+
+        signIn.setAnimation(animation);
+
         signIn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+
+                view.clearAnimation();
+
                 Intent intent = new Intent(LoginActivity.this, MainActivity.class);
                 startActivity(intent);
                 finish();
--- app/src/main/java/com/msk/adopt4k/MapActivity.java
+++ app/src/main/java/com/msk/adopt4k/MapActivity.java
@@ -13,18 +13,19 @@ import com.google.android.gms.common.GooglePlayServicesClient;
 import com.google.android.gms.location.LocationClient;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 import com.msk.adopt4k.fragment.FindOzFragment;
 
 import org.json.JSONObject;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
 
-public class MapActivity extends FragmentActivity implements GooglePlayServicesClient.ConnectionCallbacks, GooglePlayServicesClient.OnConnectionFailedListener {
+public class MapActivity extends FragmentActivity {
 
-    private LocationClient mLocationClient;
-    private double lat;
-    private double lon;
     private WebView map;
 
     @Override
@@ -32,103 +33,91 @@ public class MapActivity extends FragmentActivity implements GooglePlayServicesC
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
 
-        // 위치정보 불러오기
-        mLocationClient = new LocationClient(this, this, this);
-        mLocationClient.connect();
-
         getSupportFragmentManager().beginTransaction().replace(R.id.container, new FindOzFragment()).commit();
 
 
         map = (WebView) findViewById(R.id.map);
         map.getSettings().setJavaScriptEnabled(true);
 
-        /*JsonArray jsonArray = new JsonArray();
-
-        JsonObject loc1 = new JsonObject();
-        loc1.addProperty("lat", 25.774252);
-        loc1.addProperty("lon", -80.190262);
-        jsonArray.add(loc1);
-
-        JsonObject loc2 = new JsonObject();
-        loc2.addProperty("lat", 18.46646);
-        loc2.addProperty("lon", -66.11829);
-        jsonArray.add(loc2);
-
-        JsonObject loc3 = new JsonObject();
-        loc3.addProperty("lat", 32.321384);
-        loc3.addProperty("lon", -64.75737);
-        jsonArray.add(loc3);
-
-        JsonObject loc4 = new JsonObject();
-        loc4.addProperty("lat", 25.774252);
-        loc4.addProperty("lon", -80.190262);
-        jsonArray.add(loc4);
-
-        String polygonParam = "";
+        BufferedReader reader = null;
+        StringBuffer geojson = new StringBuffer();
         try {
-            polygonParam = URLEncoder.encode(jsonArray.toString(), "utf-8");
-        } catch (UnsupportedEncodingException e) {
-            e.printStackTrace();
-        }*/
-
-        String polygonParam = "25.774252+-80.190262+18.46646+-66.11829+32.321384+-64.75737+25.774252+-80.190262";
-
-        map.loadUrl("file:///android_asset/map.html?&lat=24.886436490787712&lon=-70.2685546875&polygon="+polygonParam);
-    }
-
-    @Override
-    protected void onStart()
-    {
-        super.onStart();
-        mLocationClient.connect();
-    }
-
-    @Override
-    protected void onStop()
-    {
-        mLocationClient.disconnect();
-        super.onStop();
-    }
-
-
-    @Override
-    public void onConnected(Bundle bundle)
-    {
-        Location location = mLocationClient.getLastLocation();
-
-        if(location == null)
-        {
-            AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
-            alertDialogBuilder.setTitle("Warning");
-            alertDialogBuilder.setMessage("Check GPS is on");
-            alertDialogBuilder.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
-                @Override
-                public void onClick(DialogInterface dialogInterface, int i) {
-                    dialogInterface.dismiss();
+            reader = new BufferedReader(new InputStreamReader(getAssets().open("EastAsia.geojson")));
+            String mLine;
+            while((mLine = reader.readLine()) != null)
+            {
+                geojson.append(mLine);
+            }
+        } catch (IOException e) {
+            //log the exception
+        } finally {
+            if (reader != null) {
+                try {
+                    reader.close();
+                } catch (IOException e) {
+                    //log the exception
                 }
-            });
-            alertDialogBuilder.show();
-        }
-        else {
-            lat = location.getLatitude();
-            lon = location.getLongitude();
-            //if(map != null)
-                //map.loadUrl("file:///android_asset/map.html?&lat="+lat+"&lon="+lon);
+            }
         }
 
-    }
+        JsonObject json = (JsonObject) new JsonParser().parse(geojson.toString());
+        JsonArray areas = json.getAsJsonArray("features");
 
 
-    @Override
-    public void onDisconnected()
-    {
+        JsonObject area = areas.get(12).getAsJsonObject();
+        JsonObject property = area.get("properties").getAsJsonObject();
 
-    }
+        double cen_x = property.get("Cen_x").getAsDouble();
+        double cen_y = property.get("Cen_y").getAsDouble();
 
-    @Override
-    public void onConnectionFailed(ConnectionResult connectionResult)
-    {
 
+        JsonObject polygon = area.get("geometry").getAsJsonObject();
+        JsonArray coordinates = polygon.getAsJsonArray("coordinates");
+
+        double firstCoordX = 0.f;
+        double firstCoordY = 0.f;
+        //String polygonParam = "25.774252+-80.190262+18.46646+-66.11829+32.321384+-64.75737+25.774252+-80.190262";
+        StringBuffer polygonParam = new StringBuffer();
+
+        for(int i = 0; i < coordinates.size(); i++)
+        {
+            JsonArray coord2 = coordinates.get(i).getAsJsonArray();
+            for(int j = 0; j < coord2.size(); j++)
+            {
+                JsonArray coord3 = coord2.get(j).getAsJsonArray();
+
+                for(int k = coord3.size()-1; k >=0; k--)
+                //for(int k = 0; k < coord3.size() ; k++)
+                {
+                    if(i == 0 && j == 0 && k == coord3.size()-1) {
+                        polygonParam.append(coord3.get(k).getAsDouble());
+                        firstCoordY = coord3.get(k).getAsDouble();
+                    } else if(i == 0 && j == 0 && k == 0) {
+                        firstCoordX = coord3.get(k).getAsDouble();
+                        polygonParam.append("+"+coord3.get(k).getAsDouble());
+                    } else {
+                        polygonParam.append("+"+coord3.get(k).getAsDouble());
+                    }
+
+                    /*
+                    JsonArray coord4 = coord3.get(k).getAsJsonArray();
+                    for(int l = coord4.size()-1; l >= 0; l--)
+                    {
+                        if(i == 0 && j == 0 && k == 0 && (l == coord4.size()-1)) {
+                            polygonParam.append(coord4.get(l).getAsDouble());
+                        } else {
+                            polygonParam.append("+"+coord4.get(l).getAsDouble());
+                        }
+                    }
+                    */
+                }
+            }
+        }
+
+        polygonParam.append("+"+firstCoordY+"+"+firstCoordX);
+        //String polygonParam = "25.774252+-80.190262+18.46646+-66.11829+32.321384+-64.75737+25.774252+-80.190262";
+
+        map.loadUrl("file:///android_asset/map.html?&lat="+cen_y+"&lon="+cen_x+"&polygon="+polygonParam.toString());
     }
 
     @Override
